b82013e7c681227e52e46dc7b6ab434005dfe268
Harshad Vilas Magadum <hvmagadum04@gmail.com>
2025-03-12 18:31:34 +0530

    Removed Secret Key From File

--- src/index.js
+++ src/index.js
@@ -26,7 +26,6 @@ const app = express();
 ( async ()=> {
     try{
         const db = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        // await mongoose.connect(`mongodb+srv://MgmHarshad:Harshad1868@videotube.w4l0g.mongodb.net`)
         console.log(db.connection.host)
         app.on("error", (err)=>{
             console.log("MongoDB Connection Failed:", err);
